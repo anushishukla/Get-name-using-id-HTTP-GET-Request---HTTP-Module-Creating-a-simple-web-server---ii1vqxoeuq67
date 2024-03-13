2962ed5c5e44475eda51b4dcaf616bab1ac90af1
anushishukla <144536251+anushishukla@users.noreply.github.com>
2024-03-13 19:45:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-const app = require('./app');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const fs = require('fs');
+const express = require('express');
+const app = express();
 
 
-dotenv.config();
-//connect to DB
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('connected to DB')
-})
+const productNames = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/names.json`, 'utf-8')
+);
 
 
-app.listen(3000, () => console.log('Server running......'));
+app.use(express.json());
 
+
+app.get('/api/v1/names/:id', (req, res) => {
+    const { id } = req.params;
+    const product = productNames.find(p => p.id === parseInt(id));
+
+    if (product) {
+        res.status(200).json({
+            status: 'success',
+            message: 'Product name fetched successfully',
+            data: {
+                name: product
+            }
+        });
+    } else {
+        res.status(404).json({
+            status: 'failed',
+            message: 'Not found!'
+        });
+    }
+});
+
+module.exports = app;
