48798a8e5db84e762e3e27f4ab277c5c886e27cc
anushishukla <144536251+anushishukla@users.noreply.github.com>
2024-03-13 19:51:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,16 +2,32 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-// Importing productNames from names.json file
+
 const productNames = JSON.parse(
-    fs.readFileSync(`${__dirname}/data/names.json`)
+    fs.readFileSync(`${__dirname}/data/names.json`, 'utf-8')
 );
 
-//Middlewares
-app.use(express.json())
 
-// GET endpoint for sending the products to client by id
-//Endpoint - /api/v1/names/:id
+app.use(express.json());
+
+
+app.get('/api/v1/names/:id', (req, res) => {
+    const { id } = req.params;
+    const product = productNames.find(p => p.id === parseInt(id));
+
+    if (product) {
+        res.status(200).json({
+            status: 'success',
+            message: 'Product name fetched successfully',
+            data: { name: product }
+        });
+    } else {
+        res.status(404).json({
+            status: 'failure',
+            message: 'Not found!'
+        });
+    }
+});
 
 
 module.exports = app;
